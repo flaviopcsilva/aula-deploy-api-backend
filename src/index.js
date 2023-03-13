9def0024a3e73f210c4670cd10f25c58ff69464e
flaviopcsilva <flaviopc2@gmail.com>
2023-03-13 20:34:28 -0300

    Pequena alteração

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', async (req, res) => {
-    return res.json('Api está ok!')
+    return res.json('Api está ok! Parabéns')
 })
 
 const port = process.env.PORT || 3000
